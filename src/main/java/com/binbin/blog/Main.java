1c673e3d1a955365f2e02784eaa21c48dd94facf
冯浩斌 <haobin.feng@southgis.com>
2024-02-21 16:46:11 +0800

    First commit

---
+++ src/main/java/com/binbin/blog/Main.java
@@ -0,0 +1,12 @@
+package com.binbin.blog;
+
+/**
+ * @Author: Feng HaoBin
+ * @Date: 2024/02/21/16:26
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
